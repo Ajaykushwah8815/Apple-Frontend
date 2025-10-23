205934906967191aaf2f0fc1cb58b491a7f2352f
Ajaykushwah8815 <135729303+Ajaykushwah8815@users.noreply.github.com>
2025-10-23 19:59:04 +0530

    Update Login.jsx

--- src/Component/Login.jsx
+++ src/Component/Login.jsx
@@ -24,7 +24,7 @@ const Login = () => {
 
     const handleLogin = async () => {
         try {
-            const res = await fetch("/api/login", {
+            const res = await fetch(`${BACK_URL}/api/login`, {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(form),
